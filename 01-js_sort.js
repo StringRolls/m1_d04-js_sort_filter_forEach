2693af14ab3b34cd1e1ef3517850dcc32eb218f8
marcosantonastasi <marco.santonastasi@gmail.com>
2022-01-20 12:23:02 +0100

    done filter

--- 01-js_sort.js
+++ 01-js_sort.js
@@ -1,1 +1,51 @@
 // Class code examples
+
+const numbers = [22, 23, 99, 68, 1, 0, 9, 112, 223, 64, 18];
+
+console.log("before:" , numbers)
+
+numbers.sort()
+
+console.log("after: ", numbers)
+
+console.log(typeof numbers[0]) // Elements stay as numbers 
+
+console.log(numbers === numbers.sort()) // Array stays the same "box"
+
+// Passing the sorting criteria using a fucntion
+
+// Rules are that TWO elements will be given to your funciton and your funciton MUST return -1, 0 or 1 accoridn to wether el1 is smaller equal or greater the el2
+
+function sortMyNumbers( el1, el2){
+    if(el1 < el2) return 1
+    if(el1 === el2) return 0
+    if(el1 > el2) return -1
+}
+
+function quickSort(el1, el2){
+    return el1 - el2
+}
+
+const sortedNumbers = numbers.sort( sortMyNumbers )
+
+console.log("sortedNumbers: ", sortedNumbers)
+
+
+const students = [
+	{firstName: 'John', lastName: 'Carr', age: 34},
+  {firstName: 'Leonardo', lastName: 'Di Vittorio', age: 21},
+  {firstName: 'Sarah', lastName: 'O\'Connor', age: 45},
+]
+
+function sortStudentsByAge(student1, student2){
+    if(student1.age < student2.age ) return -1
+    if(student1.age === student2.age) return 0
+    if(student1.age > student2.age) return 1
+}
+
+function sortStudentsByAgeQuick(student1, student2){
+    return parseInt(student1.age) - parseInt(student2.age)
+}
+
+
+console.log("studnetws by age: ", students.sort(sortStudentsByAge))
--- 02-js_filter.js
+++ 02-js_filter.js
@@ -1,1 +1,71 @@
-// Class code examples
+// Class code examplesx
+
+const numbers = [1, 60, 112, 123, 100, 99, 73, 35, 22, 23, 99, 68, 1, 0, 9, 112, 223, 64, 18]
+
+const filterEvenNumbers = function(element) {
+ if(element % 2 === 0) return true
+ else return false
+}
+
+const filterOddNumbers = function(element) {
+    if(element % 2 !== 0) return true
+    else return false
+}
+
+
+const evenNumbers = numbers.filter( filterEvenNumbers )
+
+const oddNumbers = numbers.filter( filterOddNumbers )
+
+console.log(
+    numbers,
+    evenNumbers,
+    oddNumbers
+)
+
+console.log( numbers === evenNumbers)
+console.log( numbers === oddNumbers)
+
+
+
+const getLessThan100 = function(number) {
+    return number < 100
+}
+
+const getGreaterThan100 = function(number){
+    return number > 100
+}
+
+
+const numbersLessThan100 = numbers.filter( getLessThan100 )
+const numbersGreaterThan100 = numbers.filter( getGreaterThan100 )
+
+console.log(
+    numbersLessThan100,
+    numbersGreaterThan100
+)
+
+const students = [
+    {name: 'Rafa', color: 'Blue', age: 24},
+    {name: 'Elise', color: 'Pink', age: 21},
+    {name: 'Bodei', color: 'Yellow', age: 31},
+    {name: 'Jorge', color: 'Red', age: 18}
+    ]
+
+
+const getOlderThan21 = function(student){
+    return student.age >= 21
+}
+
+const drinkingStudents = students.filter( getOlderThan21 )
+
+
+const getPinkStudent = function(student){
+    return student.color.toLowerCase() === "pink"
+}
+const pinkStudents = students.filter( getPinkStudent )
+
+console.log(
+    drinkingStudents,
+    pinkStudents
+    )
